f6f4a07247fbc34e34adce31355e228ba2f25272
me <avos.mail.dev@gmail.com>
2022-10-19 22:59:31 +0200

    Added sluggable implementation to Ingredient model. Added fillable title as well.

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -5,7 +5,28 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// soft deletes
+use Illuminate\Database\Eloquent\SoftDeletes;
+// soft deletes
+
+// spatie sluggable
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+// spatie sluggable
+
 class Ingredient extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    use HasSlug;
+
+    protected $fillable = ['title'];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
