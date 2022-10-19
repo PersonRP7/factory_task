db44d9322fca59a1e324fc5c2712842e93b40bb5
me <avos.mail.dev@gmail.com>
2022-10-19 22:57:08 +0200

    Added softDeletes, title and slug to create_ingredients_table

--- database/migrations/2022_10_19_205424_create_ingredients_table.php
+++ database/migrations/2022_10_19_205424_create_ingredients_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('ingredients', function (Blueprint $table) {
             $table->id();
+            // soft deletes
+            $table->softDeletes();
+            // soft deletes
+
+            $table->string('title');
+
+            // Spatie sluggable
+            $table->string('slug');
+            // Spatie sluggable
             $table->timestamps();
         });
     }
