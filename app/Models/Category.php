7d38f65a32d2c1408908f50558db9f974343915f
me <avos.mail.dev@gmail.com>
2022-10-19 19:46:34 +0200

    Added softDeletes trait to Category model as well as the intial migration.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// soft deletes
+use Illuminate\Database\Eloquent\SoftDeletes;
+// soft deletes
+
 class Category extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 }
