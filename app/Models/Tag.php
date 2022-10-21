fcc420f603516480ced654fffdd70f97973e3a38
me <avos.mail.dev@gmail.com>
2022-10-21 16:58:43 +0200

    Initial model setup for Tag.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// spatie sluggable
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+// spatie sluggable
+
 class Tag extends Model
 {
     use HasFactory;
+    use HasSlug;
+
+    protected $fillable = [
+        'title'
+    ]
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
