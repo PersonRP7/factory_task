4f50d12d2bfe8f804fe7796da436e21854f04971
me <avos.mail.dev@gmail.com>
2022-10-21 23:56:04 +0200

    app\Data\MealDataGenerator implemented diffTimeComparison

--- app/Data/MealDataGenerator.php
+++ app/Data/MealDataGenerator.php
@@ -8,17 +8,24 @@ use Carbon\Carbon;
 
 class MealDataGenerator
 {
-    public static function diffTimeComparison($queryParameter)
+   
+    //  Meal::whereDate('created_at', '<', $t1)->get();
+
+    //unixTimestamp = 1493902343
+    //$cls = 'Meal'
+    //$time_stamp = 'created_at', 'updated_at', 'deleted_at'
+    public static function diffTimeComparison($unixTimestamp, $cls, $time_stamp)
     {
-        if (!ctype_digit($queryParameter))
+        if (!ctype_digit($unixTimestamp))
         {
             return false;
         }
-        // return true;
-
+        $carbonObject = Carbon::createFromTimestamp($unixTimestamp);
+        return $cls::whereDate($time_stamp, '<', $carbonObject)->get();
     }
 
     // 1493902343
+    // 1697902398
     public static function getStrFromUnixT($unixTimestamp)
     {
         return Carbon::createFromTimestamp($unixTimestamp)->toDateTimeString(); 
