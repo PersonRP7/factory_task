b663508dfe178f89af414a5153b6474600d8e502
me <avos.mail.dev@gmail.com>
2022-10-21 17:24:41 +0200

    create_tag_meal migration

--- database/migrations/2022_10_21_151556_create_tag_meal.php
+++ database/migrations/2022_10_21_151556_create_tag_meal.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('tag_meal', function (Blueprint $table) {
             $table->id();
+
+            // meal fk
+            $table->unsignedBigInteger('meal_id');
+            $table->foreign('meal_id')->references('id')->on('meals')->onDelete('cascade');
+            // meal fk
+
+            // tag fk
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            // tag fk
+ 
             $table->timestamps();
         });
     }
