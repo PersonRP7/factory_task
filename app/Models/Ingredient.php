703a1122f10752ef9ba9ab5b4715b6239f39124f
me <avos.mail.dev@gmail.com>
2022-10-22 23:51:51 +0200

    Ingredient model initial setup. Ingredients Model initial setup.

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -5,7 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// spatie sluggable
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+// spatie sluggable
+
 class Ingredient extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'title'
+    ];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
