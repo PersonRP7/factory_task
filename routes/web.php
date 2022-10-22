2ef70569613aae8c471c7ecc231579b95c7bf053
me <avos.mail.dev@gmail.com>
2022-10-22 17:07:17 +0200

    Initial route setup.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('user/{name?}', function ($name=null) {  
+//     return $name;  
+// });  
+
+Route::get('with/{with?}', function ($with=null) {  
+    // return $with;
+    $values = explode(',', $with);
+    return $values;  
+});  
+
